074a17819508be04683e514550b6c8277e23dc3c
imanoliri <imanol.irizar13@gmail.com>
2022-04-20 22:26:03 +0200

    study country parameters vs time

--- notebooks/analysis_by_country.ptc.py
+++ notebooks/analysis_by_country.ptc.py
@@ -13,12 +13,44 @@ InteractiveShell.ast_node_interactivity = "all"
 #%% [markdown]
 # # Analysis by country
 #
+#%% [markdown]
+# Load simple study and plot parameters vs time by country
 #%%
-from groupby import CovidCountryStudyGroupby
 from study import CovidCountryStudy
+import matplotlib.pylab as plt
 
 st = CovidCountryStudy.from_csv('./../data/timeseries_by_country.csv',
                                 downsampling=7)
+
+min_datapoints_in_country = 10
+
+param_groups_vs_time = st.study_groups
+for country in set(st.data.index.get_level_values(1)):
+    for param_group in param_groups_vs_time:
+        # Get df only for country, the group of parameters and remove index level for the country
+        df_to_plot = st.data.loc[
+            st.data.index.get_level_values(1) == country,
+            param_group].reset_index('administrative_area_level_1').drop(
+                columns=['administrative_area_level_1'])
+
+        # Skip if empty, all values are null or not minimum data points
+        if df_to_plot.empty:
+            continue
+        if df_to_plot.isnull().values.all():
+            continue
+        if len(df_to_plot) < min_datapoints_in_country:
+            continue
+
+        # Plot
+        plt.figure()
+        ax = df_to_plot.plot(title=country)
+        plt.show()
+
+#%% [markdown]
+# Create CovidCountryStudyGroupby with interesting data aggregations & localizations
+#%%
+from groupby import CovidCountryStudyGroupby
+
 st_gb = CovidCountryStudyGroupby.from_data(
     data=st.data,
     standard_parameter_groupbys=[(max, 'icu'), (mean, 'icu'), (max, 'deaths')],
